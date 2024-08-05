30c6bab2e93d714faa7966efa982b3e172c2da06
Wahyu Pranata <wahyupranata131@gmail.com>
2024-08-06 01:59:07 +0800

    Move: data to dedicated file

---
+++ js/pkkmb-fakultas.data.js
@@ -0,0 +1,320 @@
+export const pkkmbFakultasTitles = [
+  "PKKMB Fakultas Ilmu Budaya Universitas Udayana Tahun 2024",
+  "PKKMB IUSTITIA 2024",
+  "PKKMB FT",
+  "PKKMB Fakultas Pertanian",
+  "PKKMB Fakultas Ekonomi dan Bisnis Universitas Udayana 2024",
+  "PKKMB GOPALA Udayana",
+  "PKKMB Prabu FMIPA 2024",
+  "Insan Muda Veteriner",
+  "PKKMB FTP 2024",
+  "PKKMB Ksatria Parayana",
+  "PKKMB Grafis( Gema Restorasi FISIP)",
+  "OMBAK FKP 2024",
+];
+
+export const pkkmbFakultasTimelines = [
+  "30-31 Agustus 2024",
+  "4-5 September 2024",
+  "26-27 Agustus 2024",
+  "19-20 Agustus 2024",
+  "19-20 Agustus 2024",
+  "19-20 Agustus 2024",
+  "28-29 Agustus 2024",
+  "22-23 Agustus 2024",
+  "19-20 Agustus 2024",
+  "16 & 19-20 Agustus 2024",
+  "16 & 20 Agustus 2024, 1-2 September 2024",
+  "19-20 Agustus 2024",
+];
+
+export const pkkmbFakultasDescs = [
+  "PKKMB FIB UNUD merupakan tahapan dalam penerimaan mahasiswa baru yang dilakukan oleh pimpinan fakultas di lingkungan Universitas Udayana yang didukung dan dibantu oleh unsur sivitas akademika seperti dosen, tenaga kependidikan, dan mahasiswa. Kegiatan ini dilaksanakan secara rutin setiap tahun akademik untuk mempersiapkan mahasiswa baru dalam proses transisi dan mempercepat proses adaptasi mahasiswa dengan lingkungan yang baru, serta memberikan bekal untuk keberhasilannya menempuh pendidikan di Perguruan Tinggi.",
+  "PKKMB IUSTITIA merupakan salah satu dari 3 Kegiatan wajib di fakultas Hukum Universitas Udayana yang mewajibkan mahasiswa FH untuk melaksanakan kegiatan sebagai salah satu syarat Kelulusan mahasiswa di FH, pada kesempatan kali ini PKKMB IUSTITIA menjunjung tema “SATUKAN ASA KEBERAGAMAN : WUJUDKAN GENERASI EMAS DALAM SEMANGAT PERJUANGAN “, yang dimana esensi tema ini sebagai wadah untuk berekspresi dalam menyatukan sebuah harapan dengan kepercayaan dalam luasnya keberagaman perbedaan yang ada untuk mewujudkan generasi muda yang progresif,aktif dan berkualitas sebagai implementasi peran mahasiswa  PKKMB tahun ini akan dibawakan dengan konsep humanis demi mengikuti perkembangan zaman modern yang mengaruskan melaraskan kebiasaan era modern yang berkonsepkan fleksibel tetapi tetap dalam jalur yang tegas.",
+  "PKKMB FT 2024 atau Pengenalan Kehidupan Kampus bagi Mahasisiwa Baru Fakultas Teknik Universitas Udayana 2024 merupakan suatu kegiatan yang menjadi rangkaian penerimaan mahasiswa baru Fakultas Teknik Universitas Udayana yang menjadi wajah awal sebuah Fakultas bagi pandangan mahasiswa barunya. Pada kegiatan ini menjadi pendahuluan bagi mahasiswa baru mengenal kehidupan akademik, kegiatan kemahasiswaan, organisasi maupun target awal mahasisiwa baru menentukan langkah yang ingin diambil di lingkungan Fakultas Teknik, Universitas Udayana.",
+  "Kegiatan PKKMB Fakultas Pertanian Universitas Udayana merupakan wadah pengenalan kehidupan kampus bagi mahasiswa baru dimana seluruh peserta yang berasal dari berbagai latar belakang dapat bertemu didalam naungan Fakultas Pertanian Universitas Udayana. Sebagai langkah awal mahasiswa baru dalam mengenal kehidupan kampus coklat Fakultas Pertanian Universitas Udayana.",
+  "Pengenalan Kehidupan Kampus Mahasiswa Baru (PKKMB) Fakultas Ekonomi dan Bisnis Universitas Udayana 2024 adalah bagian dari kegiatan sosialisasi almamater tingkat fakultas yang WAJIB diikuti oleh seluruh mahasiswa baru serta mahasiswa lama yang belum mengikuti ataupun mengulang (tidak lulus pada kegiatan sebelumnya) sebagai peserta. Dalam kegiatan PKKMB ini akan mengenalkan jajaran Dekanat, Senat, Prodi, dan Unit Layanan yang ada di lingkungan Fakultas Ekonomi dan Bisnis Universitas Udayana. Disajikan pula materi tentang kebangsaan, akademik, fasilitas kampus dan kemahasiswaan, Global Engagement Program (GEP), serta sharing session oleh mahasiswa Fakultas Ekonomi dan Bisnis Universitas Udayana.",
+  "Gopala merupakan wujud Dewa Krisna yang merupakan sosok penggembala sapi sedari kecil. Dari masa kecil krisna kita bisa menyimpulkan sama halnya dengan kita seorang MAHASISWA yang sedang mencari pengalaman melalui berbagai ilmu dan juga adaptasi lingkungan.",
+  "PKKMB Prabu FMIPA merupakan salah satu program kerja Bidang Kaderisasi dan Pengembangan Sumber Daya Mahasiswa (KPSDM) BEM Fakultas Matematika dan Ilmu Pengetahuan Alam. PKKMB Prabu FMIPA 2024 ini diperuntukkan bagi mahasiswa baru Fakultas Matematika dan Ilmu Pengetahuan Alam Universitas Udayana. Kegiatan ini bertujuan untuk mengenalkan kehidupan kampus serta lingkungan Fakultas baik dari segi sarana dan prasarana, jajaran dosen, lembaga mahasiswa, dan kegiatan-kegiatan kemahasiswaan yang ada di Fakultas Matematika dan Ilmu Pengetahuan Alam serta menjadi momentum untuk seluruh mahasiswa baru dapat mengenal satu sama lain. Konsep kegiatan PKKMB Prabu FMIPA 2024 yaitu menciptakan mahasiswa baru FMIPA Universitas Udayana yang bermoral, dapat beradaptasi, dan berjiwa toleransi yang tinggi guna menciptakan generasi unggul dalam mencapai kesuksesan pendidikan. Konsep ini akan diimplementasikan dalam pembekalan materi pengenalan etika dalam perkuliahan, pemberian tugas, serta pengenalan civitas academica dan lembaga mahasiswa di Fakultas Matematika dan Pengetahuan Alam.",
+  "Perkenalan Kehidupan Kampus Mahasiswa Baru merupakan suatu program wajib yang di ikuti oleh mahasiswa baru dan merupakan kegiatan untuk memperkenalkan dan mempersiapkan para mahasiswa baru berproses untuk menjadi mahasiwa yang unggul, mandiri dan berbudaya. Sehingga bisa mempercepat adaptasi mahasiswa dengan lingkungan yang baru dan memberikan bekal untuk keberhasilannya menempuh pendidikan di perguruan tinggi.",
+  "PKKMB (Pengenalan Kehidupan Kampus bagi Mahasiswa Baru) FTP (Fakultas Teknologi Pertanian) adalah sebuah kegiatan yang diadakan oleh Fakultas Teknologi Pertanian di Universitas Udayana. Tujuan dari kegiatan ini adalah untuk memperkenalkan mahasiswa baru kepada kehidupan kampus, termasuk budaya akademik, fasilitas, kegiatan kemahasiswaan, serta aturan dan tata tertib yang berlaku di Fakultas Teknologi Pertanian Universitas Udayana.",
+  "Pengenalan Kehidupan Kampus Mahasiswa Baru (PKKMB) Ksatria Parayana Fakultas Pariwisata bertujuan untuk memberikan gambaran kepada mahasiswa baru tentang dunia yang akan digelutinya nanti yaitu bidang pariwisata dan kehidupan mahasiswa di perguruan tinggi. Diharapkan para mahasiswa baru akan sedikit lebih tahu tentang pariwisata dan akan lebih mencintai bidang yang dipilihnya, dalam rangka mengembangkan pariwisata bertanggung jawab serta pengabdian terhadap bangsa dan negara. Dalam rangkaian PKKMB Ksatria Parayana terdapat pra dan hari puncak selama 2 hari. Dalam PKKMB Ksatria Parayana mahasiswa baru akan dibekali materi - materi tentang kehidupan kampus serta pengenalan lingkungan kampus dalam tingkat fakultas.",
+  "PKKMB GRAFIS tahun 2024 mengambil tema “Navigate the Uncharted”. Dalam Bahasa Inggris Navigate the Uncharted secara harafiah berarti menavigasi dan mencari tahu sesuatu yang belum diketahui keberadaannya. Tema ini bercerita mengenai seorang mahasiswa baru ibaratkan sebuah kertas putih yang kosong ketika dihadapkan mengenai dunia perkuliahan. PKKMB Grafis sebagai program pertama yang akan menyambut mahasiswa baru di Fakultas Ilmu Sosial dan Ilmu Politik yang berfungsi sebagai wadah untuk menyalurkan informasi seputar dunia kampus yang kelak akan menjadi bekal bagi mahasiswa baru untuk memulai perjalanannya di Fakultas Ilmu Sosial dan Ilmu Politik Udayana. Dengan diangkatnya tema Navigate the Uncharted sebagai tema besar PKKMB Grafis tahun 2024, tertanam harapan bahwa PKKMB Grafis dapat menjadi kompas sekaligus pemandu arah bagi mahasiswa baru angkatan 2024 untuk menavigasi perjalanan mereka menuju kehidupan baru di dunia perkuliahan.",
+  "\"OMBAK FKP 2024\" adalah nama yang diusung fakultas kami pada tahun ini, dan nama \"OMBAK\" Juga telah dikenal para mahasiswa FKP pada saat mengikuti orientasi fakultas \"Orientasi Mahasiswa Baru Kelautan dan Perikanan\" merupakan kepanjangan dari dari OMBAK FKP 2024, kata \"OMBAK\" Juga mengarah pada gelombang tinggi, kata ini diusung dengan harapan Mahasiswa Baru FKP dapat menjadi gelombang tinggi yang mampu menerobos inovasi dan kondisi dari semua keterbatasan."
+];
+
+export const pkkmbFakultasContacts = [
+  {
+    whatsapp: [
+      {
+        number: "6281949689181",
+        name: "Nadya"
+      },
+      {
+        number: "6281338769424",
+        name: "Gek Cindy"
+      }
+    ],
+    line: [
+      {
+        id: "ndgracia",
+        name: "Nadya"
+      },
+      {
+        id: "taizidianxia",
+        name: "Gek Cindy"
+      },
+    ],
+    instagram: [
+      {
+        username: "pkkmbfibunud"
+      }
+    ]
+  },
+  {
+    whatsapp: [
+      {
+        number: "6281228781613",
+        name: "Humas",
+      },
+      {
+        number: "6282145383145",
+        name: "Ketua",
+      },
+      {
+        number: "0812-4611-7878",
+        name: "Sekretaris"
+      }
+    ],
+    line: [],
+    instagram: [
+      {
+        username: "pkkmbiustitia2024"
+      }
+    ],
+  },
+  {
+    whatsapp: [
+      {
+        number: "6281999028021",
+        name: "",
+      },
+    ],
+    line: [
+      {
+        id: "8nia1ws4",
+        name: "",
+      },
+      {
+        id: "Niaarmyy",
+        name: "",
+      }
+    ],
+    instagram: [
+      {
+        username: "smft_unud"
+      }
+    ],
+  },
+  {
+    whatsapp: [
+      {
+        number: "6281936344183",
+        name: "Roka",
+      },
+      {
+        number: "6281236337502",
+        name: "Intan",
+      },
+    ],
+    line: [
+      {
+        id: "rokarocky",
+        name: "Roka",
+      },
+      {
+        id: "intanaristya_",
+        name: "Intan",
+      },
+    ],
+    instagram: [
+      {
+        username: "pkkmbfpunud"
+      }
+    ],
+  },
+  {
+    whatsapp: [
+      {
+        number: "6287703240044",
+        name: "Dayu Sri",
+      },
+    ],
+    line: [
+      {
+        id: "pirgogurlpigooh",
+        name: "Dayu Sri",
+      },
+    ],
+    instagram: [
+      {
+        username: "bemfebunud"
+      },
+      {
+        username: "mediafebunud"
+      },
+    ],
+  },
+  {
+    whatsapp: [
+      {
+        number: "6285228170895",
+        name: "Febri",
+      },
+      {
+        number: "6289685693689",
+        name: "Wini",
+      },
+    ],
+    line: [
+      {
+        id: "",
+        name: "",
+      },
+    ],
+    instagram: [
+      {
+        username: "bemkmfapetunud"
+      }
+    ],
+  },
+  {
+    whatsapp: [
+      {
+        number: "6285175385405",
+        name: "Tita",
+      },
+    ],
+    line: [
+      {
+        id: "2tictac",
+        name: "Tita",
+      },
+    ],
+    instagram: [
+      {
+        username: "prabufmipa"
+      }
+    ],
+  },
+  {
+    whatsapp: [
+      {
+        number: "6287761830618",
+        name: "Rayka",
+      },
+    ],
+    line: [
+      {
+        id: "",
+        name: "",
+      },
+    ],
+    instagram: [
+      {
+        username: "insanmudaveteriner"
+      }
+    ],
+  },
+  {
+    whatsapp: [
+      {
+        number: "6289669761697",
+        name: "Devina",
+      },
+    ],
+    line: [
+      {
+        id: "devinatwinn",
+        name: "Devina",
+      },
+    ],
+    instagram: [
+      {
+        username: "pkkmbftpunud"
+      }
+    ],
+  },
+  {
+    whatsapp: [
+      {
+        number: "6281337504796",
+        name: "Ifa",
+      },
+    ],
+    line: [
+      {
+        id: "ifarosari",
+        name: "Ifa",
+      },
+    ],
+    instagram: [
+      {
+        username: "bemfpar_unud"
+      }
+    ],
+  },
+  {
+    whatsapp: [
+      {
+        number: "6285737402070",
+        name: "Jayanthi Candra",
+      },
+    ],
+    line: [
+      {
+        id: "xyzxyz28",
+        name: "Jayanthi Candra",
+      },
+    ],
+    instagram: [
+      {
+        username: "grafis_2024"
+      }
+    ],
+  },
+  {
+    whatsapp: [
+      {
+        number: "6281236594828",
+        name: "Elyn Sukma",
+      },
+    ],
+    line: [
+      {
+        id: "",
+        name: "",
+      },
+    ],
+    instagram: [
+      {
+        username: "pkkmbfkpunud"
+      }
+    ],
+  }
+]
+
+export const pkkmbFakultasImages = [
+  "../assets/img/pkkmb-fakultas/fib.png",
+  "../assets/img/pkkmb-fakultas/fh.png",
+  "../assets/img/pkkmb-fakultas/ft.png",
+  "../assets/img/pkkmb-fakultas/fp.png",
+  "../assets/img/pkkmb-fakultas/feb.png",
+  "../assets/img/pkkmb-fakultas/fapet.png",
+  "../assets/img/pkkmb-fakultas/fmipa.png",
+  "../assets/img/pkkmb-fakultas/fkh.png",
+  "../assets/img/pkkmb-fakultas/ftp.png",
+  "../assets/img/pkkmb-fakultas/fpar.png",
+  "../assets/img/pkkmb-fakultas/fisip.png",
+  "../assets/img/pkkmb-fakultas/fkp.png",
+]
